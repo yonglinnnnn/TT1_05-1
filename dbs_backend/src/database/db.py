6718988d8560e0243abbd8446901f775fca11fe7
jacquescogal <jacques.tracy@gmail.com>
2024-01-20 10:25:26 +0800

    feat: created db.py

---
+++ dbs_backend/src/database/db.py
@@ -0,0 +1,23 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+class Database:
+    instance=None
+    def __init__(self):
+        self.connection = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+      )
+        pass
+    
+    def get_instance(cls):
+        if cls.instance==None is None:
+            cls.instance=Database()
+        return cls.instance
+    
+    def get_connection(self):
+        return self.connection
